14c882e5861e40d75bb3c1b6ed171f8ec588ab07
rishu123456 <rishu25112001@gamil.com>
2025-02-27 19:35:41 +0530

    initial portfolio done

--- src/App.tsx
+++ src/App.tsx
@@ -233,7 +233,7 @@ function App() {
           <div className="md:w-1/2 flex justify-center">
             <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-white shadow-xl">
               <img
-                src="src\profile.jpeg"
+                src="Images\profile.jpeg"
                 alt="Profile"
                 className="w-full h-full object-cover"
               />
